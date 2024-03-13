541aef61104bb3c9ed90f3ac75f6ce2421e54ecd
Niksune <47556429+Niksune@users.noreply.github.com>
2024-03-13 12:55:25 +0100

    Little upgrade of bouncing back (not enough) + adds rotation on sprite

--- Scenes/Ball/Ball.gd
+++ Scenes/Ball/Ball.gd
@@ -6,12 +6,15 @@ var direction := Vector2(1.0,1.0)
 
 @export var max_x = 852
 @export var max_y = 648
+var rotation_speed = 2.0
 
 func _init():
 	position.x = randi()%852
 	position.y = randi()%648
 
 func _physics_process(_delta):
+	
+	rotation += rotation_speed * _delta
 	
 	if(position.x < 0 or position.x >= max_x):
 		direction = Vector2(-direction.x, direction.y)
@@ -29,8 +32,15 @@ func _physics_process(_delta):
 func bonk(collision : KinematicCollision2D):
 		print("BONK ! I collided with ", collision.get_collider().name)
 		print("x : ",collision.get_collider().position.x," y : ",collision.get_collider().position.y)
+		print("dir : ",collision.get_collider().direction)
 		
-		direction = Vector2(-direction.x, -direction.y)
+		if direction.x != collision.get_collider().direction.x:
+			direction = Vector2(-direction.x, direction.y)
+			
+		if direction.y != collision.get_collider().direction.y:
+			direction = Vector2(direction.x, -direction.y)
+			
+		EVENTS.ball_bonk.emit()
 
 	
 	
