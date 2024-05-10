f4f6b4496c6aea6b30faed0517eab9447435459f
Niksune <47556429+Niksune@users.noreply.github.com>
2024-05-10 16:22:50 +0200

    Little refacto. Now that collision impact is calculated before move_and_slide, the bug is less present...

--- Scenes/Ball/Ball.gd
+++ Scenes/Ball/Ball.gd
@@ -15,9 +15,15 @@ func _init():
 	position.y = randi()%648
 
 func _physics_process(_delta):
+	manage_wall_bounce()
+	manage_collision()
 	
 	rotation += rotation_speed * _delta
+	velocity = direction * speed
+	move_and_slide()
 	
+	
+func manage_wall_bounce():
 	if(position.x < 0 and direction.x < 0):
 		direction = Vector2(-direction.x, direction.y)
 		EVENTS.ball_wall_bounce.emit()
@@ -30,20 +36,15 @@ func _physics_process(_delta):
 	if(position.y >= max_y and direction.y > 0):
 		direction = Vector2(direction.x, -direction.y)
 		EVENTS.ball_wall_bounce.emit()
-	
-	velocity = direction * speed
-	move_and_slide()
-	
+
+func manage_collision():
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
 		bonk(collision)
 
 func bonk(collision : KinematicCollision2D):
-
 	var name_str = self.get_instance_id()
 	var other_name_str = collision.get_collider().get_instance_id()
-	
-	print("BONK ! ",name_str," collided with ",other_name_str," at ",Time.get_unix_time_from_system())
 	
 	if(!collision_board.has(name_str)):
 		collision_board[name_str] = {}
@@ -56,13 +57,8 @@ func bonk(collision : KinematicCollision2D):
 		collision_board[name_str][other_name_str] = Time.get_unix_time_from_system()
 		change_direction(collision)
 		count_bonk(name_str, other_name_str)
-			
-
 
 func change_direction(collision : KinematicCollision2D):
-	
-	print("Direction changed !")
-	
 	if direction.x <= collision.get_collider().direction.x:
 		direction = Vector2(-direction.x, direction.y)
 		
@@ -71,16 +67,12 @@ func change_direction(collision : KinematicCollision2D):
 
 
 func count_bonk(name, other_name):
-	
 	var tmp
-	
 	if(other_name < name):
 		tmp = name
 		name = other_name
 		other_name = tmp
 		
-	print("name:",name," other_name:",other_name)
-	
 	if(!GLOBAL.bonk_counted_board.has(name)):
 		GLOBAL.bonk_counted_board[name] = {}
 	
